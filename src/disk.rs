3334fc457b35f5ceef027a3b0cb49163cb86211d
becker <github@deathbyescalator.com>
2021-07-02 20:23:22 -0700

    add center and base circle
    number is distance from the center.

--- src/disk.rs
+++ src/disk.rs
@@ -7,24 +7,31 @@ fn disk_image(change_list: &[i32], out_file: &str, dimension: &str) {
     let root = BitMapBackend::new(&file, (5000, 5000)).into_drawing_area();
 
     root.fill(&WHITE).unwrap();
+    root.draw(&Circle::new(
+        (2500, 2500),
+        50,
+        Into::<ShapeStyle>::into(&GREEN).filled(),
+    ))
+    .unwrap();
+
+    root.draw(&Circle::new(
+        (2500, 2500),
+        2000,
+        Into::<ShapeStyle>::into(&RED).stroke_width(1),
+    ))
+    .unwrap();
 
     let mut chart = ChartBuilder::on(&root)
         .build_cartesian_2d(-5000.0..5000.0, -5000.0..5000.0)
         .unwrap();
     let section = 360.0 / (change_list.len()) as f64;
-    let mut changes = 0;
     let point_list = change_list
         .iter()
         .enumerate()
         .map(|(i, current_point)| {
-            let next_point = change_list
-                .get(i + 1)
-                .unwrap_or_else(|| change_list.get(i).unwrap());
-            let change = current_point - next_point;
-            changes += change;
             let angle = f64::from(i as i32) * section;
             let pi_angle = (PI * 2.0 * angle) / 360.0;
-            let radius = f64::from(changes as i32 + 4000);
+            let radius = f64::from(*current_point as i32 + 4000);
             (radius * (pi_angle).cos(), radius * (pi_angle).sin())
         })
         .collect::<Vec<_>>();
@@ -46,13 +53,14 @@ pub fn make_disks(points: &[Point], dimensions: &Point, out_file: &str) {
     let middle_y = dimensions.y / 2;
     let changes_x = points
         .iter()
-        //.step_by(10)
+        //.step_by(5)
         .map(|point| point.x - middle_x)
         .collect::<Vec<_>>();
     disk_image(&changes_x, out_file, "x");
     let changes_y = points
         .iter()
-        .map(|point| middle_y - point.y)
+        //.step_by(5)
+        .map(|point| point.y - middle_y)
         .collect::<Vec<_>>();
 
     disk_image(&changes_y, out_file, "y");
